9952b1c0ec166de914c6d35d85ea0d7e839ac5f0
Kevin Coakley <kcoakley@sdsc.edu>
2020-12-16 14:25:37 -0800

    make the grid the default app at /

--- fgt/urls.py
+++ fgt/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("grid/", include("grid.urls")),
+    path("", include("grid.urls")),
     path("admin/", admin.site.urls),
 ]
